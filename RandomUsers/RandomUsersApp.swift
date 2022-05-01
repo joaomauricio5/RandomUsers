9628aae348f9b662e1aa6c74e00895cda76d8ac1
Joao Mauricio <jfmauricio5@gmail.com>
2022-05-01 20:53:48 +0100

    First commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Joao Mauricio on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
